b06d6133f6036cfc229f746e02992f52d28cb58f
Alexandr Borodinov <yaborodinov@gmail.com>
2022-10-02 20:45:01 +0300

    add LineChart, add styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BarChart } from './components/BarChart';
+import { LineChart } from './components/LineChart';
 import './App.css';
 import { useState } from 'react';
 import { UserData } from './Data'
@@ -9,12 +10,16 @@ function App() {
     labels: UserData.map(data => data.year),
     datasets: [{
       label: 'Users gained',
-      data: UserData.map(data => data.userGain)
+      data: UserData.map(data => data.userGain),
+      backgroundColor: ["rgba(137, 113, 211, 0.2)", "#D3F3ED", "#D8EFFA", "#FFEECC", "#FFDECC", "#FBD3D3", "#F5D0F8", "#CFF9FE"],
+      borderColor: "#ccc",
+      borderWidth: 1,
     }]
   })
   return ( 
     <div className="App">
-      <BarChart chartData={userData}/>
+      <BarChart chartData={userData} />
+      <LineChart chartData={userData} />
     </div>
   );
 }
---
+++ src/components/LineChart.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { Line } from 'react-chartjs-2'
+
+export const LineChart = ({ chartData })  =>  <Line data={chartData}/>
+
